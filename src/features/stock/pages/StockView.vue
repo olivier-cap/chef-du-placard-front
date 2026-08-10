24c6e8167724d3e583c2fd45416f6054398ab74c
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-08-10 23:57:29 +0200

    stock pages

---
+++ src/features/stock/pages/StockView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useGetStock } from '../composables/useGetStock';
+import Stock from '@/features/stock/components/Stock.vue'
+
+/**
+ * Page principale de l'affichage du stock.
+ * Permet la visualisation de l'état du stock: aliments présents, quantités.
+ */
+
+const {
+    stockLines,
+    getStockUser
+} = useGetStock();
+
+
+
+onMounted(
+    async () =>  await getStockUser("stock principal")
+)
+
+</script>
+
+<template>
+    <Stock :stock="stockLines"/>
+</template>
