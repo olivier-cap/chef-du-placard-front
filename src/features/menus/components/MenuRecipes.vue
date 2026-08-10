73f2d73ee4a81ec6e23b846aaae7775ba2d9a9b1
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-08-10 23:47:04 +0200

    menus components

---
+++ src/features/menus/components/MenuRecipes.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Card, CardTitle, CardContent } from '@/components/ui/card';
+import router from '@/router';
+import { Duration } from 'luxon';
+import type { RecipeViewModel } from '../models/cookableMenuModels';
+
+
+const props = defineProps<{
+    recipe: RecipeViewModel
+}>()
+
+function openRecipe(id: string) {
+    router.push({
+        name:'recipe',
+        params: { id }
+    })
+}
+
+
+</script>
+
+
+<template>
+
+    <div>
+        <Card @click="openRecipe(recipe.recipeName)">
+            <CardTitle>
+                {{ recipe.recipeName }}
+            </CardTitle>
+            <CardContent>
+                durée : {{ Duration.fromISO(recipe.duration).toHuman() }}
+                difficulté : {{ recipe.difficulty }}
+            </CardContent>
+        </Card>
+    </div>
+
+
+</template>
