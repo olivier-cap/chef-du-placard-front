5d1f547a96fccc9ce92c979760e8766a678bd9b8
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-08-10 23:57:44 +0200

    components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+
 <template>
-  <HelloWorld />
+<nav class="flex gap-4 p-4">
+    <RouterLink to="/">Accueil</RouterLink>
+    <RouterLink to="/stock">
+        Stock
+    </RouterLink>
+    <RouterLink to="/recipes">
+        Recettes
+    </RouterLink>
+    <RouterLink to="/aliments">
+        Aliments
+    </RouterLink>
+    <RouterLink to="/menus">
+        Menus
+    </RouterLink>
+</nav>
+<RouterView />
+
+
 </template>
+
+
+
