1ecb9710d1151853fef4e6ab54eb7e47087b32df
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-09-10 15:29:12 +0200

    pantry straple api service

--- src/services/pantryStaplesApi.ts
+++ src/services/pantryStaplesApi.ts
@@ -1,11 +1,11 @@
 import { api } from "@/boot/axios";
 
 
-export async function getPantryStaplesStateApi(stockId: number, pantryStaplesId: number) {
+export async function getPantryStaplesStateApi(stockId: number, pantryStaplesId: number){
     const response = await api.get("/getpantrystaplesstate",
         {params:{
             stockId: stockId,
-            pantryStaplesId: pantryStaplesId
+            pantryStapleId: pantryStaplesId
         }}
     )
 
